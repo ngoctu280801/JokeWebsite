2e3cca816eaaad3635c9690a03d51b1f5c6ae056
ngoctu280801 <63794942+ngoctu280801@users.noreply.github.com>
2022-04-08 22:44:53 +0700

    update package.js

--- Joke/models/content.model.js
+++
@@ -1,9 +0,0 @@
-import db from '../utils/db.js'
-export default{
-    async findAll(){
-        return await db.select().table('sentences');
-    },
-    async updateVote(vote, id){
-        return await db('sentences').update({vote: vote}).where({id: id})
-    },
-}
